3741243ea173b559cc913daf2382f9933484a9ba
HCanber <hakan_github@canberger.se>
2015-04-16 15:40:21 +0200

    Make the HashEnrichers serialize the hash as hexadecimal string

--- src/JsonFormatters/ExceptionHashEnricher.cs
+++ src/JsonFormatters/ExceptionHashEnricher.cs
@@ -7,26 +7,37 @@ namespace Hcanber.Serilog.JsonFormatters
 	{
 		private readonly string _propertyName;
 		private readonly IExceptionHasher _hasher;
+		private readonly string _stringFormat;
 
-		public ExceptionHashEnricher(string propertyName = "ExceptionHash")
-			: this(propertyName, new DefaultExceptionHasher())
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ExceptionHashEnricher"/> class that will add 
+		/// a property containing the hash of the log event's exception (if one exists).
+		/// By default this will be a string (hexadecimal encoding of the uint value) and the property name is "ExceptionHash"
+		/// </summary>
+		/// <param name="propertyName">OPTIONAL: Name of the property. Default: "ExceptionHash"</param>
+		/// <param name="stringFormat">OPTIONAL: The string format. Set to <c>null</c> to write the hash as a number. Default: "X" hexadecimal</param>
+		public ExceptionHashEnricher(string propertyName = "ExceptionHash", string stringFormat = "X")
+			: this(propertyName, stringFormat, new DefaultExceptionHasher())
 		{
 		}
 
-		public ExceptionHashEnricher(string propertyName, IExceptionHasher hasher)
+		public ExceptionHashEnricher(string propertyName, string stringFormat, IExceptionHasher hasher)
 		{
 			_propertyName = propertyName;
 			_hasher = hasher;
+			_stringFormat = stringFormat;
 		}
 
 		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
 		{
 			var exception = logEvent.Exception;
-			if(exception!=null)
+			if(exception != null)
 			{
 				var hash = _hasher.CalculateHash(exception);
-				logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash));
-				
+				if(_stringFormat == null)
+					logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash));
+				else
+					logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash.ToString(_stringFormat)));
 			}
 		}
 	}
--- src/JsonFormatters/MessageTemplateHashEnricher.cs
+++ src/JsonFormatters/MessageTemplateHashEnricher.cs
@@ -7,24 +7,38 @@ namespace Hcanber.Serilog.JsonFormatters
 {
 	public class MessageTemplateHashEnricher : ILogEventEnricher
 	{
+		private readonly string _stringFormat;
 		private readonly string _propertyName;
 		private readonly IStringHasher _hasher;
 
-		public MessageTemplateHashEnricher(string propertyName = "MessageTemplateHash")
-			: this(propertyName, new XxHashStringHasher())
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MessageTemplateHashEnricher"/> class that will add 
+		/// a property containing the hash of the log event's message template.
+		/// By default this will be a string (hexadecimal encoding of the uint value) and the property name is "MessageTemplateHash"
+		/// </summary>
+		/// <param name="propertyName">OPTIONAL: Name of the property. Default: "MessageTemplateHash"</param>
+		/// <param name="stringFormat">OPTIONAL: The string format. Set to <c>null</c> to write the hash as a number. Default: "X" hexadecimal</param>
+		public MessageTemplateHashEnricher(string propertyName = "MessageTemplateHash", string stringFormat = "X")
+			: this(propertyName, stringFormat, new XxHashStringHasher())
 		{
 		}
 
-		public MessageTemplateHashEnricher(string propertyName, IStringHasher hasher)
+		public MessageTemplateHashEnricher(string propertyName, string stringFormat, IStringHasher hasher)
 		{
 			_propertyName = propertyName;
 			_hasher = hasher;
+			_stringFormat = stringFormat;
+
 		}
 
 		public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
 		{
 			var hash = _hasher.CalculateHash(logEvent.MessageTemplate.Text);
-			logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash));
+			if(_stringFormat == null)
+				logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash));
+			else
+				logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(_propertyName, hash.ToString(_stringFormat)));
+
 		}
 	}
 }
