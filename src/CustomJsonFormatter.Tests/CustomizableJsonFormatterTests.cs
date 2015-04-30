a7a2bbdf3d44a747dc3ef39ddb800f1597b8862f
HCanber <hakan_github@canberger.se>
2015-04-30 12:25:26 +0200

    Add a test

---
+++ src/CustomJsonFormatter.Tests/CustomizableJsonFormatterTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog.CustomJsonFormatter.Filter;
+using Serilog.Parsing;
+using Xunit;
+
+namespace Serilog.CustomJsonFormatter.Tests
+{
+	public class CustomizableJsonFormatterTests
+	{
+		[Fact]
+		public void CustomizableJsonFormatter_using_ExcludePropertiesStartingWithUnderscore_should_inline_and_not_fail_when_formatting_underlineName()
+		{
+			var propertyNames=new PropertyNames();
+			var sut = new CustomizableJsonFormatter(propertyFilter: PropertyFilters.ExcludePropertiesStartingWithUnderscore(), jsonPropertyNames:propertyNames);
+			var writer = new StringWriter();
+			var logger = new LoggerConfiguration()
+				.WriteTo.Sink(new TextWriterSink(writer, sut))
+				.CreateLogger();
+
+			//Tests that
+			// 1. message get correctly formatted
+			// 2. {_abc} gets inlined in template
+			// 3. {abc} and {_abc} don't clash
+			logger.Information("{abc} {_abc}", 42, 4711);
+			var json =(JObject) JsonConvert.DeserializeObject(writer.ToString());
+			json[propertyNames.Message].ToString().ShouldBe("42 4711");
+			json[propertyNames.MessageTemplate].ToString().ShouldBe("{abc} 4711");
+		} 
+	}
+}
---
+++ src/CustomJsonFormatter.Tests/TextWriterSink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Serilog.CustomJsonFormatter.Tests
+{
+	public class TextWriterSink : ILogEventSink
+	{
+		readonly TextWriter _textWriter;
+		readonly ITextFormatter _textFormatter;
+		readonly object _syncRoot = new object();
+
+		public TextWriterSink(TextWriter textWriter, ITextFormatter textFormatter)
+		{
+			if(textFormatter == null) throw new ArgumentNullException("textFormatter");
+			_textWriter = textWriter;
+			_textFormatter = textFormatter;
+		}
+
+		public void Emit(LogEvent logEvent)
+		{
+			if(logEvent == null) throw new ArgumentNullException("logEvent");
+			lock(_syncRoot)
+			{
+				_textFormatter.Format(logEvent, _textWriter);
+				_textWriter.Flush();
+			}
+		}
+	}
+}
